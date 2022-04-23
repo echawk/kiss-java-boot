451fabbe90251f008357c2ef74abe454479ef7bb
Ethan <ethan.hawk@valpo.edu>
2022-04-23 16:45:42 -0500

    bootstrap.sh: add

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+[ $(basename $PWD) = 'kiss-java-boot' ] || exit 1
+
+script="$(mktemp)"
+
+sed -n '/```/=' README.md \
+    | paste -d',' - - \
+    | xargs -I{} sed -n '{}p' README.md \
+    | grep -v 'shell' \
+    | sed 's/```/cd ../' > $script
+
+sh -xe $script
